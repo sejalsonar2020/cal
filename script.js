d57f3c08627dffcf3075f470c2ee409d606a6901
sejalsonar2020 <harshikasonar25@gmail.com>
2025-07-22 19:21:50 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.forms["calc"]["display"];
+    const buttons = document.querySelectorAll("input[type='button']");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const value = button.value;
+
+            if (value === "AC") {
+                display.value = "";
+            } else if (value === "DE") {
+                display.value = display.value.slice(0, -1);
+            } else if (value === "=") {
+                try {
+                    display.value = eval(display.value);
+                } catch {
+                    display.value = "Error";
+                }
+            } else {
+                display.value += value;
+            }
+        });
+    })
